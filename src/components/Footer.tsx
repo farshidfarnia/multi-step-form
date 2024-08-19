8a8e61428e791136b4ff5b0fc1287e4c5a63ee62
Elham Shafiei <elhamshafiei@Elhams-MacBook-Pro.local>
2024-08-19 20:47:55 +0200

    button styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import React from "react";
 
 interface FooterProps {
-  className?: string; 
+  className?: string;
   showBackButton: boolean;
   showNextButton: boolean;
   showConfirmButton?: boolean;
   onBack?: () => void;
   onSubmit?: () => void;
   onConfirm?: () => void;
-
 }
 
-const Footer: React.FC<FooterProps> = ({ className = '', showBackButton, showNextButton, showConfirmButton, onBack, onSubmit, onConfirm }) => {
+const Footer: React.FC<FooterProps> = ({
+  className = "",
+  showBackButton,
+  showNextButton,
+  showConfirmButton,
+  onBack,
+  onSubmit,
+  onConfirm,
+}) => {
   return (
     <div className={`footer ${className}`}>
-      {showBackButton && <button type="button" className="back-btn" onClick={onBack}>Go Back</button>}
-      {showNextButton && <button type="submit" className="next-step-btn" onClick={onSubmit}>Next Step</button>}
-      {showConfirmButton && <button type="button" className="confirm-btn" onClick={onConfirm}>Confirm</button>}
+      {showBackButton && (
+        <button type="button" className="back-btn" onClick={onBack}>
+          Go Back
+        </button>
+      )}
+      {showNextButton && (
+        <button type="submit" onClick={onSubmit}>
+          Next Step
+        </button>
+      )}
+      {showConfirmButton && (
+        <button type="button" className="confirm-btn" onClick={onConfirm}>
+          Confirm
+        </button>
+      )}
     </div>
   );
-}
+};
 
 export default Footer;
